bd5893d1319899f1e923b4fa7105c28659cccce4
Akalugo Daniel <akalugidaniel@gmail.com>
2022-08-25 23:30:14 +0100

    Implemented the ability to send voicenotes images and files in this chatapp

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Turbochat
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.after_initialize do |_config|
+      User.update_all(status: User.statuses[:offline])
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
